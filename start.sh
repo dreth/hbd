9e96ab6e1244c35ad42586010808a44767dca930
dreth <almost_a_nightmare@pm.me>
2024-07-27 21:23:07 +0200

    fix hbd db creation launch command in start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ PORT=${PORT:-8418}
 cd /app/backend
 
 # if 'hbd.db' does not exist, create it as an empty file
-if [ ! -f hbd.db ]; then
+if [ ! -f /app/data/hbd.db ]; then
     touch /app/data/hbd.db
 fi
 
